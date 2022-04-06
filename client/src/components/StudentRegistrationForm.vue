7f545774c6b1b0278a77b13e4a7bcd56dd4c654b
Magezi <arafats144@gmail.com>
2022-04-06 11:10:31 +0300

    node practical code

---
+++ client/src/components/StudentRegistrationForm.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1>Student Registration form goes here</h1>
+</template>
+
+<script>
+export default {
+    name:"StudentRegistrationForm",
+    data(){
+        return{}
+    },
+    methods:{},
+    // mounted(){
+
+    // },
+    components:{}
+}
+</script>
+<style>
+
+</style>
